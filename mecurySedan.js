17a17946e3ecb712e615d2fcb101837d19bd2a34
Aaron Everette <aeverette0120@gmail.com>
2022-07-27 20:35:08 -0400

    finished mecurySedan.js

--- mecurySedan.js
+++ mecurySedan.js
@@ -19,8 +19,37 @@ class Car extends VehicleModule {
         }
     }
 
-    start
+    start() {
+        if (this.fuel > 0) {
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+        if ((num + this.passenger) <= this.maxPassengers) {
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log(this.model + "" + this.make + " not have enough seats for all of the passengers.");
+
+        }
+    } else {
+        console.log(thismodel + "" + this.make + "is full"); 
+    }
+
+    }
+
 }
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let myV = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "blue", "30000");
+
+myV.start()
+myV.loadPassenger(5)
+myV.stop()
+myV.checkService()
+console.log(myV)
